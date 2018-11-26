51788a86ce151874178518f91ee53725edc8ccaf
wang kai hua <570759907@qq.com>
2018-11-26 21:35:25 +0800

    完成文章引用标签功能

--- src/components/EditorArticleContainer.vue
+++ src/components/EditorArticleContainer.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    {{this.$route.params.id}}
     <el-form label-width="40px">
       <el-form-item label="标题">
         <el-input placeholder="请输入文章标题" v-model="blog.title" size="medium"></el-input>
       </el-form-item>
       <el-form-item label="分类">
-        <el-select v-model="blog.categoryId" placeholder="请选择">
+        <el-select v-model="blog.categoryId" placeholder="请选择分类" size="medium">
           <el-option
             v-for="category in categoryList"
             :key="category.id"
             :label="category.name"
             :value="category.id">
+          </el-option>
+        </el-select>
+        </el-form-item>
+        <el-form-item label="标签">
+        <el-select v-model="blog.tags" multiple placeholder="请选择标签" size="medium">
+          <el-option
+            v-for="tag in tagList"
+            :key="tag.id"
+            :label="tag.name"
+            :value="tag.id">
           </el-option>
         </el-select>
       </el-form-item>
@@ -23,9 +32,6 @@
         </router-link>
       </el-form-item>
     </el-form>
-
-    <!-- <mavon-editor :toolbarsFlag="false" :editable="false" 
-    :subfield="false" :defaultOpen="preview" v-model="value"/> -->
   </div>
   
 </template>
@@ -35,12 +41,14 @@
     data() {
       return {
         blog: {},
-        categoryList: []
+        categoryList: [],
+        tagList: [],
       }
     },
     created() {
       this.getCategoryList();
       this.getArticle();
+      this.getTagList();
     },
     methods: {
       // 新增或者修改
@@ -67,13 +75,20 @@
         });
       },
 
+      // 回显标签列表
+      getTagList(){
+        this.$http.get("tag/findAll")
+        .then(result => {
+          this.tagList = result.body;
+        });
+      },
+
       // 修改 文章内容回显
       getArticle() {
         if(this.$route.params.id){
           this.$http.get("blog/findOne?id="+this.$route.params.id)
             .then(result => {
               this.blog = result.body;
-              console.log(result.body)
           });
         }
       }
--- src/components/TagsContainer.vue
+++ src/components/TagsContainer.vue
@@ -1,15 +1,90 @@
 <template>
   <div>
-    links
-    
+    <el-form :inline="true">
+      <el-form-item label="添加标签">
+        <el-input v-model="tag.name" size="mini" placeholder="请输入标签名称"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" size="mini" @click="addtag()">保存</el-button>
+      </el-form-item>
+    </el-form>
+    <hr>
+    <el-table :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+      style="width: 100%">
+      <el-table-column label="标签名称" prop="name">
+      </el-table-column> 
+      <el-table-column  align="right">
+        <template slot="header" slot-scope="scope">
+          <el-input
+            v-model="search"
+            size="mini"
+            placeholder="输入关键字搜索"/>
+        </template>
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-</script>
+  import moment from 'moment'
+  export default {
+    data() {
+      return {
+        tableData: [],
+        search: '',
+        tag: {},
+      }
+    },
+    created() {
+      this.getTagList();
+    },
+    methods: {
+      // 获取所有标签
+      getTagList(){
+        this.$http.get("tag/findAll")
+        .then(result => {
+          this.tableData = result.body;
+        });
+      },
+      
+      handleEdit(index, row) {
+        console.log(index, row);
+      },
 
+      // 添加标签
+      addtag(){
+        this.$http.post("tag/add",this.tag)
+        .then(result => {
+          if(result.body.success){
+            this.tag = {}
+            this.getTagList()
+          }
+        });
+      },
+      // 删除标签
+      handleDelete(index, row) {
+        console.log(index, row);
+         this.$http.get("tag/delete?ids="+row.id)
+        .then(result => {
+          if(result.body.success){
+            this.getTagList()
+          }
+        });
+      },
+    }
+    
+  }
+</script>
 
 <style lang="scss" scoped>
 
- 
 </style>
